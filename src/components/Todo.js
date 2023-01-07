5b98dfa414d462c37669041a20753f277c61491b
jayeshcholkar <115981022+jayeshcholkar@users.noreply.github.com>
2023-01-07 23:17:32 +0530

    first comt

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -25,7 +25,7 @@ function Todo() {
   };
 
   return (
-    <div className="h-100 w-full flex items-center justify-center font-sans">
+    <div className="h-200 w-full flex items-center justify-center font-sans">
       <div className="bg-white mt-[5%] rounded-md shadow-xl p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
         <div className="mb-4">
           <h1 className=" text-center font-semibold text-2xl">Todo List</h1>
@@ -38,14 +38,15 @@ function Todo() {
             />
             <button
               onClick={taskList}
-              className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-green-500 ease-in duration-200"
+              className="flex-no-shrink p-2 border-2 rounded text-teal 
+              border-teal hover:text-white hover:bg-green-500 ease-in duration-200"
             >
               Add
             </button>
           </div>
         </div>
         <div>
-          <div className="">
+          <div >
             <ol>
               {lists.map((list, index) => {
                 return (
